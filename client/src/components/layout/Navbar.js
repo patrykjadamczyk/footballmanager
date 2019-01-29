cb330d1b7012c2ec76602d5020179a5a1541bd8e
Patryk Adamczyk <patrykadamczyk@patrykadamczyk.net>
2019-01-29 11:59:01 +0100

    Brak zmian

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -70,7 +70,7 @@ class Navbar extends Component {
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
         <div className="container">
           <Link className="navbar-brand" to="/">
-            Football Manager &copy; 2019
+            Football Manager
             {isAuthenticated ? (
               <span className="badge ml-3 badge-light">{user.name}</span>
             ) : (
